5d79f49403108bb37edb86b35af2b4b89ee952d6
suraj1947 <pabhilash777@gmail.com>
2025-05-18 00:07:20 +0530

    Defining controller User & Bug fixes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,66 @@
-import { asyncHandler } from "../utils/asyncHandler.js"
-
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "./../utils/ApiError.js";
+import { ApiResponse } from "./../utils/ApiResponse.js";
+import { User } from "./../models/user.models.js";
+import { uploadOnCloudinary } from "./../utils/cloudinary.js";
 
 const registerUser = asyncHandler(async (req,res)=>{
-  res.status(200).json({
-    message:"User Ok"
-  })
+    // 1. Get user details from the frontend.
+      const { fullname, email, password, username } = req.body;
+
+    // 2. Check for validations based on ifEmpty, type etc.
+      if(
+        [fullname,email,password,username].some(field=>field.trim()==="")
+      ){
+        throw new ApiError(400,"All fields are required");
+      }
+
+    // 3. Check if the user is already present.
+      const existingUser = User.findOne({
+        $or: [{username},{email}]
+      })
+      console.log(existingUser)
+      if(existingUser){
+        throw new ApiError(409,"User with this email or username already exists.")
+      }
+
+    // 4. Check if the images, especially avatar is present.
+      const avatarLocalPath = req.files?.avatar[0]?.path;
+      const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+      if(!avatarLocalPath){
+        throw new ApiError(400,"Avatar is required.");
+      }
+      
+      // 5. Upload them to cloudinary.
+      const avatar = await uploadOnCloudinary(avatarLocalPath);
+      const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+      
+      if(!avatar){
+        throw new ApiError(400,"Avatar is required.");
+      }
+    
+      // 6. Save the data to User table.
+    const user = await User.create({
+      fullname,
+      avatar: avatar.url,
+      coverImage: coverImage.url || "",
+      email,
+      password,
+      username: username.toLowerCase()
+    });
+    
+      // 7. Remove the password and refresh token from the response.
+    const createdUser = await User.findById(user._id).select(
+      "-password -refreshToken"
+    );
+
+    // 8. Send the response back to the user.
+    if(!createdUser){
+      throw new ApiError(500,"Something went wrong while registering a User.")
+    }
+
+    return res.status(201).json(new ApiResponse(200,createdUser,"User Registered successfully."))
 })
 
 export { registerUser };
--- src/models/user.models.js
+++ src/models/user.models.js
@@ -46,7 +46,7 @@ const userSchema = new Schema({
 userSchema.pre("save",async function(next){
   if(!this.isModified("password")) return next();
 
-  bcrypt.hash(this.password,10);
+  await bcrypt.hash(this.password,10);
   next();
 });
 
